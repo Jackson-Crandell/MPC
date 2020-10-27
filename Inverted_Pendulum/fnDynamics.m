f2bc94ebdb3dd0e197ab8a419f593f3db1ad16d9
Jackson-Crandell <jackson.crandell@mac.com>
2020-10-27 19:02:22 -0400

    Added Robust Code

---
+++ Inverted_Pendulum/fnDynamics.m
@@ -0,0 +1,19 @@
+% Propogating nominal dynamics
+function [x] = fnDynamics(xo,u_new,Horizon,dt,sigma,dynamics)
+
+global g;
+global m; 
+global l; 
+global I; 
+global b; 
+
+x = xo;
+
+for k = 1:(Horizon-1)
+      Fx(1,1) = x(2,k); 
+      Fx(2,1) = ((-b/I)*x(2,k))-((m*g*l)/I)*sin(x(1,k));
+
+      G_x(2,1) = 1/I;
+
+x(:,k+1) = x(:,k) + Fx * dt + G_x * u_new(:,k) * dt + G_x * u_new(:,k) * sqrt(dt) * sigma * randn; %Creates trajectory based on dynamics
+end
